9d7a8e20bc12e23df130069864025aea4cb20b00
Desarrollo <mmolina@fca.uncu.edu.ar>
2026-04-21 13:32:49 -0300

    Se agrego pagina de blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+APP_DIRS=True
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
